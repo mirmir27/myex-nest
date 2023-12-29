0089093a9967346ed9566815cbb335a8f8b2b4c9
mirmir27 <miraclerumondor27@gmail.com>
2023-12-29 09:40:19 +0800

    Delete

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -26,5 +26,11 @@ export class UsersController {
     findUser(@Param('id') id : string) {
         return this.userService.findOneBy(parseInt(id));
     }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id : string ){
+        return this.userService.remove(parseInt(id));
+    }
+
 
 }
